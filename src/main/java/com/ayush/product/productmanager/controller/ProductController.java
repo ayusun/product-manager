d3460653e7f337656e1cbf31d5d566885c144f61
Ayush Choubey <ayush.choubey@gmail.com>
2018-06-26 13:45:35 +0800

    adding jwt auth

--- src/main/java/com/ayush/product/productmanager/controller/ProductController.java
+++ src/main/java/com/ayush/product/productmanager/controller/ProductController.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -21,6 +22,7 @@ public class ProductController {
     private final ProductService productService;
 
     @PutMapping(value = "/")
+    @PreAuthorize("hasRole('USER')")
     public ResponseEntity createProduct(@RequestBody PutProductVo createProductEvent){
         List<ProductVO> products = createProductEvent.getProducts();
         try{
---
+++ src/main/java/com/ayush/product/productmanager/entity/UserRoleName.java
@@ -0,0 +1,6 @@
+package com.ayush.product.productmanager.entity;
+
+public enum UserRoleName {
+    ROLE_USER,
+    ROLE_ADMIN
+}
